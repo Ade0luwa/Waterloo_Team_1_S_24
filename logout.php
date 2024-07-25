51a20a710aecf78b916b64c0bedbabb3943a46e2
Ade0luwa <136064501+Adeoluwatomi@users.noreply.github.com>
2024-07-24 20:50:07 -0400

    Add .gitignore and refactored logout, signup.php

--- logout.php
+++ logout.php
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-<?php 
-    session_start();
-    session_unset();
-    session_destroy();
-    header('Location: index.php');  
-    exit;
-?>
-=======
-=======
->>>>>>> 2b8c92e9d2cd7c5e8e178b8ca5bcde885ac0c468
 <?php
 session_start();
 session_unset();
@@ -18,8 +6,3 @@ session_start();
 $_SESSION['logout_success'] = true;
 header('Location: login.php');
 exit;
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
-?>
->>>>>>> 2b8c92e9d2cd7c5e8e178b8ca5bcde885ac0c468
